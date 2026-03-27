5a35550a9a6b1163c797363fe40c61f7feacf70d
JoaoPedro1808 <jpedrossantana@gmail.com>
2026-03-27 16:47:03 -0300

    solid

---
+++ solid/src/cleancode/Pedido.java
@@ -0,0 +1,28 @@
+package cleancode;
+import java.util.List;
+
+public class Pedido {
+    public int id;
+    public Cliente cliente;
+    public List<Item> itens;
+    public double total;
+    public String status;
+
+    public double precoTotal() {
+        double subTotal = 0;
+        for (Item item : itens) {
+            subTotal += item.calcularPrecoTotal();
+        }
+        return subTotal;
+    }
+
+    public void detalhesPedido() {
+        System.out.println("Pedido " + id);
+        System.out.println("Cliente " + cliente.nome);
+
+        for (Item iten : itens) {
+            System.out.println(iten.nome);
+        }
+        System.out.println(total);
+    }
+}
